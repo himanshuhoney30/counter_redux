fde39ac1a1cb4474cd03b5903200b11455c1a532
himanshuhoney30 <himanshuhoney30@gmail.com>
2024-06-14 18:13:11 +0530

    Second commit

--- project/src/Components/Counter.js
+++ project/src/Components/Counter.js
@@ -9,7 +9,7 @@ function Counter() {
         <div>
             <div className="" style={{ width: "50rem", height: "10rem" }}>
                 <div class="">
-                    <h1 class="">Counter={data}</h1>
+                    <h1 class="">Counter:{data}</h1>
                     <div className='d-flex gap-2 justify-content-center'>
                         <button type='submit' className='btn btn-primary' onClick={()=>dispatch(increment())}>Increment</button>
                         <button type='submit' className='btn btn-danger' onClick={()=>dispatch(showdata())}>Reset</button>
